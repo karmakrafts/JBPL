04b5c12616397ec43059baf632240c408694a853
KitsuneAlex <alexh@karmakrafts.dev>
2025-10-18 07:23:05 +0200

    Implement identity evaluation for AnonBlockExpr

--- jbpl-assembler/src/main/java/dev/karmakrafts/jbpl/assembler/model/expr/AnonBlockExpr.java
+++ jbpl-assembler/src/main/java/dev/karmakrafts/jbpl/assembler/model/expr/AnonBlockExpr.java
@@ -1,8 +1,13 @@
 package dev.karmakrafts.jbpl.assembler.model.expr;
 
+import dev.karmakrafts.jbpl.assembler.AssemblerContext;
 import dev.karmakrafts.jbpl.assembler.model.ScopeOwner;
 import dev.karmakrafts.jbpl.assembler.model.statement.AbstractStatementContainer;
+import org.jetbrains.annotations.NotNull;
 
 public final class AnonBlockExpr extends AbstractStatementContainer implements Expr, ScopeOwner {
-
+    @Override
+    public @NotNull Expr evaluate(final @NotNull AssemblerContext context) {
+        return this; // Anon blocks evaluate to themselves
+    }
 }
