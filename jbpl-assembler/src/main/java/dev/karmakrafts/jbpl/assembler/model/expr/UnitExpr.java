1e565610694ce44e88dbb1eebb80dd4d6cb48b1e
KitsuneAlex <alexh@karmakrafts.dev>
2025-10-18 07:19:39 +0200

    Implement referential-as-structural-equality for UnitExpr

--- jbpl-assembler/src/main/java/dev/karmakrafts/jbpl/assembler/model/expr/UnitExpr.java
+++ jbpl-assembler/src/main/java/dev/karmakrafts/jbpl/assembler/model/expr/UnitExpr.java
@@ -1,7 +1,23 @@
 package dev.karmakrafts.jbpl.assembler.model.expr;
 
+import dev.karmakrafts.jbpl.assembler.AssemblerContext;
 import dev.karmakrafts.jbpl.assembler.model.AbstractElement;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 public final class UnitExpr extends AbstractElement implements Expr {
+    @Override
+    public @NotNull Expr evaluate(final @NotNull AssemblerContext context) {
+        return this; // Unit expression evaluate to themselves
+    }
 
+    @Override
+    public boolean equals(final @Nullable Object obj) {
+        return obj instanceof UnitExpr;
+    }
+
+    @Override
+    public String toString() {
+        return "UnitExpr";
+    }
 }
