41eb1bb95fa1d640419b451ea3e9317b338c1a8c
KitsuneAlex <alexh@karmakrafts.dev>
2025-10-18 22:05:29 +0200

    Remove custom impls of equals/toString from ArrayType record

--- jbpl-assembler/src/main/java/dev/karmakrafts/jbpl/assembler/model/type/ArrayType.java
+++ jbpl-assembler/src/main/java/dev/karmakrafts/jbpl/assembler/model/type/ArrayType.java
@@ -2,7 +2,6 @@ package dev.karmakrafts.jbpl.assembler.model.type;
 
 import dev.karmakrafts.jbpl.assembler.AssemblerContext;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
 public record ArrayType(Type elementType, int dimensions) implements Type {
     public ArrayType(final @NotNull Type elementType, final int dimensions) {
@@ -37,18 +36,5 @@ public record ArrayType(Type elementType, int dimensions) implements Type {
     @Override
     public @NotNull ArrayType array(final int dimensions) {
         return new ArrayType(elementType, this.dimensions + dimensions);
-    }
-
-    @Override
-    public boolean equals(final @Nullable Object obj) {
-        if (!(obj instanceof ArrayType arrayType)) {
-            return false;
-        }
-        return elementType.equals(arrayType.elementType) && dimensions == arrayType.dimensions;
-    }
-
-    @Override
-    public @NotNull String toString() {
-        return String.format("ArrayType[elementType=%s,dimensions=%d]", elementType, dimensions);
     }
 }
