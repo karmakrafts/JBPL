53bbc880a09525f1ac174bb411daca970801c9e2
KitsuneAlex <alexh@karmakrafts.dev>
2025-11-11 22:00:50 +0100

    Add support for types and strings to in-expression

--- jbpl-assembler/src/main/java/dev/karmakrafts/jbpl/assembler/model/expr/InExpr.java
+++ jbpl-assembler/src/main/java/dev/karmakrafts/jbpl/assembler/model/expr/InExpr.java
@@ -18,13 +18,14 @@ package dev.karmakrafts.jbpl.assembler.model.expr;
 
 import dev.karmakrafts.jbpl.assembler.eval.EvaluationContext;
 import dev.karmakrafts.jbpl.assembler.eval.EvaluationException;
-import dev.karmakrafts.jbpl.assembler.model.type.ArrayType;
-import dev.karmakrafts.jbpl.assembler.model.type.BuiltinType;
-import dev.karmakrafts.jbpl.assembler.model.type.Type;
+import dev.karmakrafts.jbpl.assembler.model.type.*;
 import dev.karmakrafts.jbpl.assembler.source.SourceDiagnostic;
+import dev.karmakrafts.jbpl.assembler.util.ExceptionUtils;
 import org.jetbrains.annotations.NotNull;
 
 import java.lang.reflect.Array;
+import java.util.HashSet;
+import java.util.stream.Collectors;
 
 public final class InExpr extends AbstractExprContainer implements Expr {
     public static final int LHS_INDEX = 0;
@@ -58,12 +59,67 @@ public final class InExpr extends AbstractExprContainer implements Expr {
     @Override
     public void evaluate(final @NotNull EvaluationContext context) throws EvaluationException {
         final var lhsValue = getLhs().evaluateAsConst(context, Object.class);
+        final var lhsType = getLhs().getType(context);
         final var rhsValue = getRhs().evaluateAsConst(context, Object.class);
         final var rhsType = getRhs().getType(context);
-        // For arrays, we check if the given lhs value is present in the array
-        if (rhsType instanceof ArrayType arrayType) {
+        if (rhsType == BuiltinType.STRING) {
+            context.pushValue(LiteralExpr.of(rhsValue.toString().contains(lhsValue.toString())));
+            return;
+        }
+        else if (rhsType == PreproType.FUNCTION_SIGNATURE) {
+            final var signature = (FunctionSignatureExpr) rhsValue;
+            if (lhsType == BuiltinType.STRING) {
+                context.pushValue(LiteralExpr.of(signature.getFunctionName().evaluateAsConst(context,
+                    String.class).equals(lhsValue)));
+                return;
+            }
+            else if (lhsType == PreproType.TYPE) {
+                // @formatter:off
+                final var types = signature.getExpressions().stream()
+                    .map(ExceptionUtils.unsafeFunction(expr -> expr.getType(context)))
+                    .collect(Collectors.toSet());
+                // @formatter:on
+                context.pushValue(LiteralExpr.of(types.contains((Type) lhsValue)));
+                return;
+            }
+        }
+        else if (rhsType == PreproType.FIELD_SIGNATURE) {
+            final var signature = (FieldSignatureExpr) rhsValue;
+            if (lhsType == BuiltinType.STRING) {
+                // @formatter:off
+                final var name = signature.getExpressions().stream()
+                    .filter(ExceptionUtils.unsafePredicate(expr -> expr.getType(context) == BuiltinType.STRING))
+                    .findFirst();
+                // @formatter:on
+                context.pushValue(LiteralExpr.of(name.isPresent() && name.get().evaluateAsConst(context,
+                    String.class).equals(lhsValue)));
+                return;
+            }
+            else if (lhsType == PreproType.TYPE) {
+                // @formatter:off
+                final var types = signature.getExpressions().stream()
+                    .map(ExceptionUtils.unsafeFunction(expr -> expr.getType(context)))
+                    .collect(Collectors.toSet());
+                // @formatter:on
+                context.pushValue(LiteralExpr.of(types.contains((Type) lhsValue)));
+            }
+        }
+        else if (rhsValue instanceof IntersectionType rhsIntersectionType) {
+            if (lhsValue instanceof IntersectionType lhsIntersectionType) {
+                context.pushValue(LiteralExpr.of(new HashSet<>(rhsIntersectionType.alternatives()).containsAll(
+                    lhsIntersectionType.alternatives()), getTokenRange()));
+                return;
+            }
+            else if (lhsValue instanceof Type lhsTypeValue) {
+                context.pushValue(LiteralExpr.of(rhsIntersectionType.alternatives().contains(lhsTypeValue),
+                    getTokenRange()));
+                return;
+            }
+            context.pushValue(LiteralExpr.of(false, getTokenRange()));
+            return;
+        }
+        else if (rhsType instanceof ArrayType arrayType) {
             final var elementType = arrayType.elementType();
-            final var lhsType = getLhs().getType(context);
             if (!elementType.isAssignableFrom(lhsType)) {
                 throw new EvaluationException(String.format("Element type %s cannot appear in array of type %s",
                     lhsType,
